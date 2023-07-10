ecca03867af2e26bc3d5a6a1e8654313621d73a0
Ertan Kayalar <ertan@ertankayalar.com>
2023-07-10 16:11:06 +0300

    scripts-qtile-rm-picom

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,7 +8,7 @@ feh --bg-scale ~/Wallpapers/mike-enerio-XsH3wpZzBKc-unsplash.jpg &
 # feh --bg-scale ~/.config/backgrounds/Wallpaperkiss_1208315.jpg &
 
 # compositor
-picom --config ~/.config/picom/picom.conf &
+#picom --config ~/.config/picom/picom.conf &
 
 # sxhkd
 sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
