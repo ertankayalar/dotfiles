537a5153b799bb46dba483f664dfe77f5a215d99
ertan kayalar <ertan@ertankayalar.com>
2023-07-13 17:02:10 +0300

    colors-jagl

---
+++ .config/qtile/colors_jagl.py
@@ -0,0 +1,29 @@
+
+colors = {
+	'bg':   		'#1e1e2e',
+	'fg':			'#cdd6f4',
+    'dark-red':     '#eba0ac',
+    'red':          '#f38ba8',
+    'dark-green':   '#94e2d5',
+    'green':        '#a6e3a1',
+    'dark-yellow':  '#fab387',
+    'yellow':       '#f9e2af',
+    'dark-blue':    '#74c7ec',
+    'blue':         '#89b4fa',
+    'dark-magenta': '#cba6f7',
+    'magenta':      '#f5c2e7',
+    'dark-cyan':    '#94e2d5',
+    'cyan':         '#89dceb',
+    'dark-gray':    '#7f849c',
+    'gray':         '#9399b2',
+
+    'fg4':          '#7f849c',
+    'fg3':          '#6c7086',
+    'fg2':          '#585b70',
+    'fg1':          '#45475a',
+    'bg0':          '#313244',
+    'fg0':          '#181825',
+    'fg9':          '#bac2de',
+    'bgt':          '#000000'
+}
+
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -10,8 +10,9 @@ import subprocess
 from libqtile import hook
 
 # color schema
-from colors import colors
+from colors_jagl import colors
 
+# test
 print("dark-green:", colors['dark-green'])
 
 @hook.subscribe.startup_once
@@ -23,37 +24,6 @@ mod = "mod4"
 #terminal = guess_terminal()
 myTerm = "alacritty" 
 myBrowser = "librewolf"
-"""
-Color schemes live here
-"""
-_jagl = {
-	'bg':   		'#1e1e2e',
-	'fg':			'#cdd6f4',
-    'dark-red':     '#eba0ac',
-    'red':          '#f38ba8',
-    'dark-green':   '#94e2d5',
-    'green':        '#a6e3a1',
-    'dark-yellow':  '#fab387',
-    'yellow':       '#f9e2af',
-    'dark-blue':    '#74c7ec',
-    'blue':         '#89b4fa',
-    'dark-magenta': '#cba6f7',
-    'magenta':      '#f5c2e7',
-    'dark-cyan':    '#94e2d5',
-    'cyan':         '#89dceb',
-    'dark-gray':    '#7f849c',
-    'gray':         '#9399b2',
-
-    'fg4':          '#7f849c',
-    'fg3':          '#6c7086',
-    'fg2':          '#585b70',
-    'fg1':          '#45475a',
-    'bg0':          '#313244',
-    'fg0':          '#181825',
-    'fg9':          '#bac2de'
-}
-
-
 
 keys = [
 
