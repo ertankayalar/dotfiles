01d7857edc7f7666a0629c74e061a9ee55b0af4e
Ertan Kayalar <ertan@ertankayalar.com>
2023-07-15 09:29:36 +0300

    nvchad-custom

---
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,66 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "json",
+    "bash",
+    "tsx",
+    "python",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "astro"
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "astro-language-server",
+    "tailwindcss-language-server",
+    "prettier",
+    "python-lsp-server",
+    "pyright",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
